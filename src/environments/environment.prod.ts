202a0f020edbdda1cfdabfbdedf9b12e5167e05e
pawarut-klaiarmon <pawarut.klaiarmon@allianz.com>
2022-08-02 09:26:08 +0700

    Remove firebase api key from project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:870991689767:web:0f067378159faa86177602',
     databaseURL: 'https://klaiarmon-management-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'klaiarmon-management.appspot.com',
-    apiKey: 'AIzaSyCq8X-O2Y97zb0yKQF7NnaIFOnYM7bAR2w',
+    apiKey: '',
     authDomain: 'klaiarmon-management.firebaseapp.com',
     messagingSenderId: '870991689767',
     measurementId: 'G-L4BC23E9QC',
