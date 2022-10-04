6a9ad83d522df803831c2504c4ed743cb1c8f477
pawarut-klaiarmon <pawarut.klaiarmon@allianz.com>
2022-10-04 23:22:28 +0700

    [expense-alert]: Able to show expense by paid by

--- src/app/core/expense-alert/expense-alert.service.ts
+++ src/app/core/expense-alert/expense-alert.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Action } from "src/app/common/enum/action";
+import { filterByEqual } from "src/app/common/utils/common-util";
 import { ExpenseModalComponent } from "src/app/pages/accounting/expense/components/expense-modal/expense-modal.component";
 import { Expense } from "src/app/pages/accounting/expense/model/expense.model";
 import { ExpenseCommonService } from "src/app/pages/accounting/expense/services/expense-common.service";
 import { ExpenseSearchingService } from "src/app/pages/accounting/expense/services/expense-searching.service";
 import { ExpenseUpdationService } from "src/app/pages/accounting/expense/services/expense-updation.service";
+import { FireAuthService } from "../services/fire-auth.service";
 
 @Injectable()
 export class ExpenseAlertService {
@@ -13,12 +15,14 @@ export class ExpenseAlertService {
     constructor(private modalService: NgbModal,
         private expenseCommonService: ExpenseCommonService,
         private expenseSearchingService: ExpenseSearchingService,
-        private expenseUpdationService: ExpenseUpdationService) {}
+        private expenseUpdationService: ExpenseUpdationService,
+        private fireAuthService: FireAuthService) {}
 
     async subscribeExpenseAlert(): Promise<void> {
         const expenseAlerts = await this.expenseSearchingService.searchByStatus('ALERT');
+        const currentUser = await this.fireAuthService.getCurrentUser();
 
-        await Promise.all(expenseAlerts.map(async expenseAlert => {
+        await Promise.all(expenseAlerts.filter(filterByEqual('paidBy', currentUser.uid!)).map(async expenseAlert => {
             const expenseFormValue = await this.expenseCommonService.getExpenseFormValueFromExpense(expenseAlert);
             const modalRef = this.modalService.open(ExpenseModalComponent, { centered: true, backdrop: 'static' });
             modalRef.componentInstance.action = Action.EXPENSE_ALERT;
